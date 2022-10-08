62271c9a4c9d61079060b6d2bc2176694fbd6df6
thuggeelya <ilya-fdmdh@mail.ru>
2022-10-08 22:30:52 +0700

    loginform repo

--- src/main/java/com/example/thuggeelya/data/security/LoginFormRepository.java
+++ src/main/java/com/example/thuggeelya/data/security/LoginFormRepository.java
@@ -15,21 +15,21 @@ import java.util.Optional;
 public interface LoginFormRepository extends JpaRepository<LoginForm, Integer> {
     Optional<LoginForm> findByLogin(@Param("login") String login);
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN') || hasRole('ADMIN')")
     @NotNull
     List<LoginForm> findAll();
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN') || hasRole('ADMIN')")
     @NotNull
     @Override
     <S extends LoginForm> S save(@NotNull S entity);
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN') || hasRole('ADMIN')")
     @Modifying(clearAutomatically = true)
     @Override
     void deleteById(@Param("id") @NotNull Integer id);
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN') || hasRole('ADMIN')")
     @Modifying(clearAutomatically = true)
     @Override
     void delete(@Param("loginForm") @NotNull LoginForm loginForm);
